dbd3018ebdfc0b19edd84c247065ac32089b6489
Andrea Civitarese <s1092302@studenti.univpm.it>
2020-12-16 17:52:19 +0100

    Creazione applicazione

---
+++ meteoOOP/src/main/java/it/ldaac/meteoOOP/MeteoOopApplication.java
@@ -0,0 +1,13 @@
+package it.ldaac.meteoOOP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeteoOopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeteoOopApplication.class, args);
+	}
+
+}
---
+++ meteoOOP/src/test/java/it/ldaac/meteoOOP/MeteoOopApplicationTests.java
@@ -0,0 +1,13 @@
+package it.ldaac.meteoOOP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeteoOopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
